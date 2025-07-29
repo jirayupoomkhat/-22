66569c5b4c80cd857bb65f5ae08807d70d3ac928
jirayupomkhat <jirayupoomiatk@gmail.com>
2025-07-29 21:52:35 +0700

    Add files via upload

---
+++ Makefile/main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+
+#include <iostream>  //เป็นไดเร็กทิฟพรีโพรเซสเซอร์ที่แจ้งให้คอมไพเลอร์รวมiostreamไลบรารี ไว้
+#include "function.h"  //บอกให้คอมไพเลอร์รวมไฟล์เฮดเดอร์ function.h เข้ามาในโปรแกรม
+using namespace std;  // ประกาศว่าจะใช้std
+
+// main() is where program execution begins.
+int main() {    //ฟั้งชันหลักของตัวcode
+   
+	print_hello();  // print_hello()
+	return 0;  //ไม่มีการย้อนกลับ
+}
